e58e089142d3cd4be8df29379337f20cd962c278
Quach Thanh Nghi <thanhnghi1301@gmail.com>
2024-06-27 21:27:57 +0700

    create listing api route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 import userRouter from './user.route.js'
 import authRouter from './auth.route.js'
+import listingRouter from './listing.route.js'
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
     app.use('/api/auth', authRouter)
+    app.use('/api/listing', listingRouter)
 
     return app.use('/', (req, res) => {
         res.send('Server on ...')
