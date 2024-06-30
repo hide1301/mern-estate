2d434e33ac4acb9dab015631f2947ee6c2c73fcb
Quach Thanh Nghi <thanhnghi1301@gmail.com>
2024-06-30 16:07:47 +0700

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,16 +3,19 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import initRoutes from './routes/index.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config()
 
 mongoose
-    .connect(process.env.DATABASE_URL)
-    .then(() => {
-        console.log('Connected to MongoDB!')
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log('Connected to MongoDB!')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+const __dirname = path.resolve()
 
 const app = express()
 
@@ -21,16 +24,22 @@ app.use(cookieParser())
 
 initRoutes(app)
 
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Internal Server Error'
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    })
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Internal Server Error'
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
 })
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+  console.log('Server is running on port 3000')
 })
